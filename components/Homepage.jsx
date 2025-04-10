b71f8bce1b3170764ee22be66b41eb6f392ffec0
ShreddShark <daniel.cox2685@gmail.com>
2025-04-09 21:26:05 -0500

    updated pricing table, and removed poster

--- components/Homepage.jsx
+++ components/Homepage.jsx
@@ -1,21 +1,20 @@
-import React, { useEffect } from "react";
-import Navigation from "./Navigation";
-import bgVideo from "../src/assets/bgVideo.mp4";
-import Poster from "./Poster";
+import React, { useEffect } from "react"
+import Navigation from "./Navigation"
+import bgVideo from "../src/assets/bgVideo.mp4"
+import PricingTable from "./PricingTable"
+import "./Homepage.css"
+import "animate.css/animate.min.css"
 
-import "./Homepage.css";
-import "animate.css/animate.min.css";
-
-import { Container, Row, Col } from "react-bootstrap";
-import Barbers from "./Barbers";
+import { Container, Row, Col } from "react-bootstrap"
+import Barbers from "./Barbers"
 
 function Homepage() {
   useEffect(() => {
-    const siteLogo = document.querySelector(".siteLogo");
+    const siteLogo = document.querySelector(".siteLogo")
     if (siteLogo) {
-      siteLogo.classList.add("animate__fadeInDown");
+      siteLogo.classList.add("animate__fadeInDown")
     }
-  }, []);
+  }, [])
 
   return (
     <Container fluid>
@@ -28,7 +27,7 @@ function Homepage() {
         </video>
 
         <Row
-          className="align-items-center justify-content-center w-100 h-100 text-white"
+          className="text-white align-items-center justify-content-center w-100 h-100"
           id="heroContent"
         >
           <Col xs={12} md={8} className="text-center">
@@ -38,7 +37,7 @@ function Homepage() {
               Classic Vibes.
             </h1>
             <h5 className="mb-4">fade, line-up, beard trim - we got you.</h5>
-            <div className="d-flex flex-wrap justify-content-center gap-3">
+            <div className="flex-wrap gap-3 d-flex justify-content-center">
               <button className="px-4 py-2 text-dark btn btn-warning fw-semibold">
                 Book Appointment
               </button>
@@ -53,7 +52,7 @@ function Homepage() {
       {/* Poster Section */}
       <Row>
         <Col>
-          <Poster />
+          <PricingTable />
         </Col>
       </Row>
 
@@ -63,7 +62,7 @@ function Homepage() {
         </Col>
       </Row>
     </Container>
-  );
+  )
 }
 
-export default Homepage;
+export default Homepage
